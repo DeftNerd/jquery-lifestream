7327306bf554f9431548cd6a3422284eae3ea6e7
Armin Roșu <armin.rosu@gmail.com>
2013-04-06 15:54:02 +0300

    code for pull request #126

--- jquery.lifestream.js
+++ jquery.lifestream.js
@@ -5,7 +5,7 @@
  *
  * Copyright 2011, Christian Vuerings - http://denbuzze.com
  */
-/*globals jQuery, $ */
+/*global jQuery */
 ;(function( $ ){
 
   "use strict";
@@ -144,7 +144,7 @@
       // At then end we call the load method.
       if( !jQuery.tmpl ) {
         jQuery.getScript(
-          "http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js",
+          "http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js",
           load);
       } else {
         load();
@@ -2277,8 +2277,9 @@ $.fn.lifestream.feeds.vimeo = function( config, callback ) {
           config: config,
           html: $.tmpl( template[type], {
             url: item.url,
-            description: item.description.replace(/"/g, "'")
-              .replace( /<.+?>/gi, ""),
+            description: item.description ? item.description
+              .replace(/"/g, "'")
+              .replace( /<.+?>/gi, "") : '',
             title: item.title
           })
         });
@@ -2430,22 +2431,43 @@ $.fn.lifestream.feeds.youtube = function( config, callback ) {
 
   var template = $.extend({},
     {
+      uploaded: 'uploaded <a href="${video.player.default}" '
+        + 'title="${video.description}">${video.title}</a>',
       favorited: 'favorited <a href="${video.player.default}" '
         + 'title="${video.description}">${video.title}</a>'
     },
     config.template),
 
-  parseYoutube = function( input ) {
-    var output = [], i = 0, j, item;
+  parseYoutube = function( input, activity ) {
+    var output = [], i = 0, j, item, video, date, templateData;
 
     if(input.data && input.data.items) {
       j = input.data.items.length;
       for( ; i<j; i++) {
         item = input.data.items[i];
+
+        switch (activity) {
+          case 'favorited':
+            video = item.video;
+            date = item.created;
+            templateData = item;
+            break;
+          case 'uploaded':
+            video = item;
+            date = video.uploaded;
+            templateData = {video: video};
+            break;
+        }
+
+        // Don't add unavailable items (private, rejected, failed)
+        if (!video.player && !video.player.default) {
+          continue;
+        }
+
         output.push({
-          date: new Date(item.created),
+          date: new Date(date),
           config: config,
-          html: $.tmpl( template.favorited, item )
+          html: $.tmpl( template[activity], templateData )
         });
       }
     }
@@ -2458,7 +2480,16 @@ $.fn.lifestream.feeds.youtube = function( config, callback ) {
       + "/favorites?v=2&alt=jsonc",
     dataType: 'jsonp',
     success: function( data ) {
-      callback(parseYoutube(data));
+      callback(parseYoutube(data, 'favorited'));
+    }
+  });
+
+  $.ajax({
+    url: "http://gdata.youtube.com/feeds/api/users/" + config.user
+      + "/uploads?v=2&alt=jsonc",
+    dataType: 'jsonp',
+    success: function( data ) {
+      callback(parseYoutube(data, 'uploaded'));
     }
   });
 
@@ -2469,7 +2500,8 @@ $.fn.lifestream.feeds.youtube = function( config, callback ) {
   };
 
 };
-})(jQuery);(function($) {
+})(jQuery);
+(function($) {
 $.fn.lifestream.feeds.zotero = function( config, callback ) {
 
   var template = $.extend({},
