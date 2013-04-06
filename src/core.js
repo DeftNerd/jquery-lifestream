7f50e59bd342aa499d68ed119d8a2a1f70608c04
Armin Roșu <armin.rosu@gmail.com>
2013-04-06 19:14:13 +0300

    thou shalt not use double quotes

--- src/core.js
+++ src/core.js
@@ -144,7 +144,7 @@
       // At then end we call the load method.
       if( !jQuery.tmpl ) {
         jQuery.getScript(
-          "http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js",
+          'http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js',
           load);
       } else {
         load();
